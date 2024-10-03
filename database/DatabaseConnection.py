dcb08f3a1de06fdfc8d96e55b24b5bb1ae445b0e
Nouster <djebali.mohamed88@gmail.com>
2024-10-03 16:36:41 +0200

    feat(db) allow env variable

--- database/DatabaseConnection.py
+++ database/DatabaseConnection.py
@@ -1,8 +1,17 @@
-# database_connection.py
+from dotenv import load_dotenv
+import os
 import pymysql.cursors
 
+load_dotenv()
+
 class DatabaseConnection:
-    def __init__(self, host='localhost', user='root', password='root', database='stock-manager', port=8889):
+    def __init__(self):
+        host = os.getenv('DB_HOST', 'localhost')
+        user = os.getenv('DB_USER', 'root')
+        password = os.getenv('DB_PASSWORD', 'root')
+        database = os.getenv('DB_DATABASE', 'stock-manager')
+        port = int(os.getenv('DB_PORT', 8889))
+
         self.connection = None
         try:
             self.connection = pymysql.connect(
